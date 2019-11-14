05e67145b9932d9ebe1ffad55e0bdd0250784336
VSemikoz <slava.semik@yandex.ru>
2019-11-14 22:10:03 +0300

    test

--- Socket.py
+++ Socket.py
@@ -2,11 +2,12 @@ import socket
 import Queue
 from threading import Thread
 from Threads import Threads
+from Message import Message
 
 class Socket:
     def __init__(self):
         self.host = '192.168.1.243'
-        self.port = 261
+        self.port = 263
         self.address = (self.host, self.port)
 
         self.threads = Threads()
@@ -17,6 +18,7 @@ class Server(Socket):
     def run_server(self):
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         udp_socket.bind(self.address)
+        udp_socket.setblocking(False)
         udp_socket.settimeout(1)
 
         server_thread = Thread(target=self.threads.server_main_cycle_thread, args=(udp_socket,))
@@ -40,9 +42,10 @@ class Client(Socket):
         udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         udp_socket.settimeout(1)
 
-        udp_socket.sendto(str.encode("Hello"), self.address)
+        msg = Message('server', 4, 'Hello server')
+        udp_socket.sendto(str(msg), self.address)
         print "start request_thread"
-        request_thread = Thread(target=self.threads.request_waiting_thread, args=(udp_socket,))
+        request_thread = Thread(target=self.threads.request_waiting_thread, args=(udp_socket, self.address))
         request_thread.start()
 
         while self.threads.client_connection:
@@ -55,12 +58,16 @@ class Client(Socket):
     def process_user_input(self, user_input, udp_socket):
         print user_input
         if user_input == "quit":
+            msg = Message('server', 1, "quit")
+            udp_socket.sendto(str(msg), self.address)
             self.threads.client_connection = False
         if user_input == "get_map":
             map = self.get_map()
             print map
         if user_input == "send_map":
             self.send_map(udp_socket)
+        if user_input == "update_map":
+            self.update_map(udp_socket)
 
     def get_map(self):
         handle = open("map.mapAI", "r")
@@ -69,7 +76,16 @@ class Client(Socket):
         return data
 
     def send_map(self, udp_socket):
-        print "sending", self.address
+        print "sending"
         map_data = self.get_map()
-        udp_socket.sendto(str.encode(map_data), self.address)
+        msg = Message('all', 3, map_data)
+        print msg
+        udp_socket.sendto(str(msg), self.address)
+
+    def update_map(self, udp_socket):
+        print "sending map update request"
+        msg = Message('all', 2, "map update request")
+        print msg
+        udp_socket.sendto(str(msg), self.address)
+
 
--- Threads.py
+++ Threads.py
@@ -1,50 +1,108 @@
 import socket
 import Queue
 from threading import Thread
+from map_storage import Map
+from Message import Message
+import ast
+
 
 class Threads():
     def __init__(self):
         self.server_connection = True
         self.client_connection = True
 
-    def request_waiting_thread(self, client_socket):
+    def request_waiting_thread(self, client_socket, address):
         messages_queue = Queue.Queue()
+        map = Map()
 
         while self.client_connection:
-                try:
-                    data, addr = client_socket.recvfrom(10240)
-                except:
-                    continue
-                print('from %s recived %s' % (addr, data))
-                messages_queue.put({addr: data})
-                if not messages_queue.empty():
-                    message_queue_process_thread = Thread(target=self.message_queue_process, args=(messages_queue,))
-                    message_queue_process_thread.start()
-                    message_queue_process_thread.join()
+            try:
+                data, addr = client_socket.recvfrom(102400)
+            except:
+                continue
+            print('from %s recived %s' % (addr, data))
+            messages_queue.put({addr: data})
+            if not messages_queue.empty():
+                message_queue_process_thread = Thread(target=self.message_queue_process,
+                                                      args=(messages_queue, client_socket, address, map))
+                message_queue_process_thread.start()
+                message_queue_process_thread.join()
 
-
-    def message_queue_process(self, messages_queue):
+    def message_queue_process(self, messages_queue, udp_socket, address, map):
         new_message = messages_queue.get()
-        print new_message,'wowow'
+        addr = new_message.keys()[0]
+        msg = new_message[addr].split('/ ')
+
+        #print addr, msg
+
+        if int(msg[1]) == 0:  # server_quit
+            self.client_connection = False
+            return
+
+        if int(msg[1]) == 2:  # map_update_request
+            map_chunks = Map().getChunkGridFormFile("map1.mapAI")
+
+            msg = Message("all", 5, dict_to_str(map_chunks))
+            udp_socket.sendto(str(msg), address)
+            return
+
+        if int(msg[1]) == 5:  # map_update_response
+
+            file_map_update(map, "map2.mapAI", msg[2])
+            return
 
     def server_main_cycle_thread(self, udp_socket):
         adrress_list = []
         messages_queue = Queue.Queue()
 
         while self.server_connection:
-                try:
-                    data, addr = udp_socket.recvfrom(10240)
-                except:
-                    continue
-                print('from %s recived %s' % (addr, data))
-                messages_queue.put({addr: data.decode("utf-8")})
+            try:
+                data, addr = udp_socket.recvfrom(102400)
+                data_list = data.split('/ ')
                 if addr not in adrress_list:
                     adrress_list.append(addr)
+                if data_list[2] == "quit":
+                    adrress_list.remove(addr)
+            except:
+                continue
+            print('from %s recived %s' % (addr, data))
+            messages_queue.put({addr: data.decode("utf-8")})
 
-                new_message = messages_queue.get()
-                if new_message:
-                    for addr in adrress_list:
-                        for sendAddr in adrress_list:
-                            if addr != sendAddr:
-                                print('Send %s to %s' % (str.encode(data), addr))
-                                udp_socket.sendto(str.encode(data), addr)
+            new_message = messages_queue.get()
+            if new_message and data_list[0] == 'all':
+                for sendAddr in adrress_list:
+                    if addr != sendAddr:
+                        print('Send %s to %s' % (str.encode(data), sendAddr))
+                        udp_socket.sendto(str.encode(data), sendAddr)
+
+        # close server-clinet connections
+        for addr in adrress_list:
+            msg = Message("all", 0, "server_quit")
+            udp_socket.sendto(str(msg), addr)
+
+
+def dict_to_str(dictation):
+    result_str = ""
+    for key in dictation.keys():
+        result_str += str(key) + ' : ' + dictation[key] + ', '
+    return result_str
+
+
+def file_map_update(map, map_file_name, response_map):
+    response_map = '{' + response_map + '}'
+    respose_map_dict = ast.literal_eval(response_map)
+    map_chunks = map.getChunkGridFormFile(map_file_name)
+
+    for key in respose_map_dict.keys():
+        map_chunks[key] = respose_map_dict[key]
+
+    map.updateChunksFromDict(map_chunks)
+    map_text = map.printToText()
+    for line_number in range(len(map_text)):
+        map_text[line_number] = ''.join(map_text[line_number])
+
+    f = open(map_file_name, "w")
+    for line in map_text:
+        f.write(line)
+        f.write('\n')
+    f.close()
